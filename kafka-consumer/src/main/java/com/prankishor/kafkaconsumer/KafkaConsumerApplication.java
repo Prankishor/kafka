5781c2836537d357b9419247aab9213c781293fc
Prankishor Talukdar <prankishortalukdar1@gmail.com>
2021-10-12 14:43:30 +0530

    Add files via upload

---
+++ kafka-consumer/src/main/java/com/prankishor/kafkaconsumer/KafkaConsumerApplication.java
@@ -0,0 +1,50 @@
+package com.prankishor.kafkaconsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.prankishor.kafkaconsumer.config.*;
+
+@SpringBootApplication
+@RestController
+public class KafkaConsumerApplication {
+	
+	List<String> messages=new ArrayList<>();
+	
+	User userFromTopic = null;
+	
+	@GetMapping("/consumeStringMessage")
+	public List<String> consumeMsg(){
+		return messages;
+	}
+
+	@KafkaListener(groupId="prankishor-1", topics="daimler", containerFactory="kafkaListenerContainerFactory")
+	public List<String> getMessageFromTopic(String data) {
+		messages.add(data);
+		return messages;
+		
+	}
+	
+	//For Json
+	
+	@GetMapping("/consumeJson")
+	public User consumeJson() {
+		return userFromTopic;
+	}
+	
+	@KafkaListener(groupId="prankishor-2", topics="daimler", containerFactory="userKafkaListenerContainerFactory")
+	public User getJsonFromTopic(User user) {
+		userFromTopic = user;
+		return userFromTopic;
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerApplication.class, args);
+	}
+
+}
